1a50f5a6df98c67a9bb0583e9fbd951f0b1d0ab5
Denis Pankov <idenispankov@gmail.com>
2021-03-08 14:28:13 -0800

    customers section created

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,40 @@
 import Carousel from "../components/Carousel.js";
 
+// Benefits
 function addBenefits() {
   const benefitsContainer = document.querySelector(".benefits__container");
   const benefitsElementTemplate = document.querySelector("#benefits-template")
     .content;
   data.benefits.map((item) => {
     const benefitsItem = benefitsElementTemplate.cloneNode(true);
-    benefitsItem.querySelector(".benefits__image-container").style.background = `url(${item.image}) center no-repeat`;
+    benefitsItem.querySelector(
+      ".benefits__image-container"
+    ).style.background = `url(${item.image}) center no-repeat`;
     benefitsItem.querySelector(".item-title").textContent = item.title;
     benefitsItem.querySelector(".item-description").textContent =
       item.description;
     benefitsContainer.append(benefitsItem);
   });
 }
+
+// Customers
+function addCustomers() {
+  const customersContainer = document.querySelector(".customers__container");
+  const customersElementTemplate = document.querySelector("#customers-template")
+    .content;
+  data.customers.map((item) => {
+    const customersItem = customersElementTemplate.cloneNode(true);
+    const img = customersItem.querySelector(".customers__image");
+    img.src = item.image;
+    img.alt = item.title;
+    customersItem.querySelector(".item-title").textContent = item.title;
+    customersItem.querySelector(".item-description").textContent =
+      item.description;
+    customersContainer.append(customersItem);
+  });
+}
+
+// Team
 function addTeam() {
   const teamContainer = document.querySelector(".team__container");
   const teamElementTemplate = document.querySelector("#team-template").content;
@@ -28,6 +50,7 @@ function addTeam() {
   });
 }
 
+// Gallery item
 function populateGalleryItem(item) {
   document.querySelector(
     ".gallery__image-container"
@@ -36,6 +59,7 @@ function populateGalleryItem(item) {
     item.description;
 }
 
+// Products Item
 function populateProductsItem(item) {
   document.querySelector(
     ".products__image-container"
@@ -57,12 +81,15 @@ function populateProductsItem(item) {
   ).textContent = `Charging time: ${item.features.charging_time}`;
 }
 
+// Gallery Carousel
 const galleryCarousel = new Carousel({
   data: data.gallery,
   btnBackClass: "gallery__button-back",
   btnForwardClass: "gallery__button-forward",
   populateItem: populateGalleryItem,
 });
+
+// Products Carousel
 const productsCarousel = new Carousel({
   data: data.products,
   btnBackClass: "products__button-back",
@@ -74,3 +101,4 @@ galleryCarousel.generateCarousel();
 productsCarousel.generateCarousel();
 addBenefits();
 addTeam();
+addCustomers();
