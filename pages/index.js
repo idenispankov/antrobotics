f75e86f014a427e97f891fe9fe8680eca1536a35
Denis Pankov <idenispankov@gmail.com>
2021-03-10 16:09:50 -0800

    updated

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,9 @@ function addBenefits() {
     .content;
   data.benefits.map((item) => {
     const benefitsItem = benefitsElementTemplate.cloneNode(true);
-    benefitsItem.querySelector(".benefits__image-container").style.background = `url(${item.image}) center no-repeat`;
+    benefitsItem.querySelector(
+      ".benefits__image-container"
+    ).style.background = `url(${item.image}) center no-repeat`;
     benefitsItem.querySelector(".item-title").textContent = item.title;
     benefitsItem.querySelector(".item-description").textContent =
       item.description;
@@ -69,6 +71,35 @@ const productsCarousel = new Carousel({
   btnForwardClass: "products__button-forward",
   populateItem: populateProductsItem,
 });
+
+// Popup
+const popup = document.querySelector(".popup");
+const inputs = document.querySelectorAll(".form__input");
+const formButton = document.querySelector(".work__button");
+
+function openPopup() {
+  popup.classList.add("popup-open");
+  inputs.forEach((input) => (input.value = ""));
+}
+
+function closePopup(e) {
+  if (
+    e.target.classList.contains("popup__form-close") ||
+    e.target.classList.contains("popup")
+  ) {
+    popup.classList.remove("popup-open");
+  }
+}
+
+function escClosePopup(e) {
+  if (e.key === "Escape") {
+    popup.classList.remove("popup-open");
+  }
+}
+
+formButton.addEventListener("click", openPopup);
+document.addEventListener("click", closePopup);
+document.addEventListener("keyup", escClosePopup);
 
 galleryCarousel.generateCarousel();
 productsCarousel.generateCarousel();
