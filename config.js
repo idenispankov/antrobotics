6e5c29ae7ce208364b6818871c1000f206179713
Denis Pankov <idenispankov@gmail.com>
2021-03-16 17:06:58 -0700

    Created Customers Section

--- config.js
+++ config.js
@@ -4,25 +4,25 @@ let data = {
       title: "Labor-intensive dolly movements",
       description:
         "Challenges: It is more and more difficult to hire workers, because they prefer to work in delivery services for the same money.",
-      image: "./assets/customers/labor-intensive.jpg",
+      image: "./assets/customers/labor-intensive.png",
     },
     {
       title: "Forklifts, tuggers managed by man",
       description:
         "Challenges: It is more and more difficult to hire forklift drivers  because they prefer to work in delivery for the same money.",
-      image: "./assets/customers/forklifts.jpg",
+      image: "./assets/customers/forklifts.png",
     },
     {
       title: "Follow-the-line robots",
       description:
         "Challenges: It is time-consuming and expensive to replace old robots by state-of-the art, but produced by another vendor.",
-      image: "./assets/customers/follow-the-line.jpg",
+      image: "./assets/customers/follow-the-line.png",
     },
     {
       title: "Autonomous mobile robots ",
       description:
         "Challenges: Limited upgrade options within the same vendor robot line only and too expensive to use another vendor robots.",
-      image: "./assets/customers/autonomous.jpg",
+      image: "./assets/customers/autonomous.png",
     },
   ],
 
